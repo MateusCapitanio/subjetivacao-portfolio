38409a660f491c9adc046c98d7076c65f4741b39
MateusCapitanio <mateusjmj92@gmail.com>
2023-06-09 13:16:50 -0300

    feat: Subindo projeto para repositório remoto

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <div className="bg-white drop-shadow-lg py-5 px-4 m-5 sm:m-10 rounded-lg">
+        <span className="font-bold text-lg">Renata Aragão</span>
+      </div>
+    </main>
+  )
+}
