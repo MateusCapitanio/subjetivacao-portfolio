b37dfbd0a3336dffc4a50078f4c0712615bc1a18
MateusCapitanio <mateusjmj92@gmail.com>
2023-06-15 20:53:06 -0300

    feat: Alteração de headerr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
 export default function Home() {
   return (
     <main>
-      <div className="bg-white drop-shadow-lg py-5 px-4 m-5 sm:m-10 rounded-lg">
+      <div className="flex justify-between bg-white drop-shadow-lg py-5 px-4 m-5 sm:m-10 rounded-lg">
         <span className="font-bold text-lg">Renata Aragão</span>
+        <ul className="flex gap-2">
+          <li>s</li>
+          <li>s</li>
+          <li>s</li>
+          <li>s</li>
+        </ul>
       </div>
     </main>
   )
