a065658414f61191912d71f0b363a14ede3011cc
Mateus Capitanio <mateusjmj92@gmail.com>
2023-06-26 22:56:22 -0300

    feat: Desenvolvendo o Header da aplicação

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,25 @@
+import Header from "@/components/Header";
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <main>
-      <div className="flex justify-between bg-white drop-shadow-lg py-5 px-4 m-5 sm:m-10 rounded-lg">
-        <span className="font-bold text-lg">Renata Aragão</span>
-        <ul className="flex gap-2">
-          <li>s</li>
-          <li>s</li>
-          <li>s</li>
-          <li>s</li>
-        </ul>
+    <main className="">
+      <Header />
+      {/*DEIXEI ESSE EXEMPLO AQUI EMBAIXO PRA PODER DAR UMA OLHADA 
+      DPS NO DESENVOLVIMENTO COM A FOTO DELA E TALS JA, TEM FOTO DELA JA NA PASTA PUBLIC*/}
+      {/* <div className="flex items-center justify-center">
+      
+      <div className="bg-green-500 w-44">
+        ashdkjasdkasjhskdhjsdh kas d askjdh askj dasjk hdjask dhsak dhas 
       </div>
+      <Image 
+        className="m-36 rounded-full"
+        alt="Contém uma imagem profissional"
+        height={420}
+        width={420}
+        src='/images/profilePhoto.png'
+      />
+      </div> */}
     </main>
   )
 }
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,51 @@
+import { BsWhatsapp } from 'react-icons/bs';
+import { AiFillLinkedin, AiOutlineInstagram } from 'react-icons/ai';
+
+export const Header = () => {
+  return (
+    <div className='flex justify-center'>
+      <header className="flex justify-between fixed bg-white drop-shadow-lg py-5 px-4 mt-10 w-full max-w-7xl rounded-lg">
+        <span className="font-bold text-lg">Renata Aragão</span>
+        <ul className="flex gap-14">
+          <div className="flex gap-2 items-center">
+            <li><a className="text-[#333333] hover:bg-[#F7D1D1] hover:text-[#333333] py-1 px-2 rounded transition duration-500" href="#">Tratamentos</a></li>
+            <li><a className="text-[#333333] hover:bg-[#F7D1D1] hover:text-[#333333] py-1 px-2 rounded transition duration-500" href="#">Serviços</a></li>
+            <li><a className="text-[#333333] hover:bg-[#F7D1D1] hover:text-[#333333] py-1 px-2 rounded transition duration-500" href="#">Para empresas</a></li>
+          </div>
+          <div className="flex gap-2 items-center">
+            <li>
+              <a
+                href="https://www.linkedin.com/in/renata-aragao"
+                target="_blank"
+                className="flex items-center drop-shadow-lg gap-1 bg-blue-600 py-1 px-2 text-white rounded"
+              >
+                <AiFillLinkedin />
+                Linkedin
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://wa.me/5521980482411"
+                target="_blank"
+                className="flex items-center drop-shadow-lg gap-1 bg-[#F7D1D1] py-1 px-2 text-[#333333] rounded">
+                <BsWhatsapp color='#333333' />
+                Whatsapp
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.instagram.com/subjetivacao"
+                target="_blank"
+                className="flex items-center drop-shadow-lg gap-1 bg-gradient-to-r from-[#FEC761] to-[#DB2E7C] py-1 px-2 text-white rounded">
+                <AiOutlineInstagram />
+                Instagram
+              </a>
+            </li>
+          </div>
+        </ul>
+      </header>
+    </div>
+  )
+}
+
+export default Header;
