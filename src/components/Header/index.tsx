9a27289ab3106eeebdb1123cbcbfcce45745dc66
Mateus Capitanio <mateusjmj92@gmail.com>
2023-06-26 23:43:53 -0300

    feat: Resolvendo TODOS erros de link

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -16,32 +16,35 @@ export const Header = () => {
           </div>
           <div className="flex gap-2 items-center">
             <li>
-              <a
+              <Link
                 href="https://www.linkedin.com/in/renata-aragao"
                 target="_blank"
+                rel='noreferrer'
                 className="flex items-center drop-shadow-lg gap-1 bg-blue-600 py-1 px-2 text-white rounded"
               >
                 <AiFillLinkedin />
                 Linkedin
-              </a>
+              </Link>
             </li>
             <li>
-              <a
+              <Link
                 href="https://wa.me/5521980482411"
                 target="_blank"
+                rel='noreferrer'
                 className="flex items-center drop-shadow-lg gap-1 bg-[#F7D1D1] py-1 px-2 text-[#333333] rounded">
                 <BsWhatsapp color='#333333' />
                 Whatsapp
-              </a>
+              </Link>
             </li>
             <li>
-              <a
+              <Link
                 href="https://www.instagram.com/subjetivacao"
                 target="_blank"
+                rel='noreferrer'
                 className="flex items-center drop-shadow-lg gap-1 bg-gradient-to-r from-[#FEC761] to-[#DB2E7C] py-1 px-2 text-white rounded">
                 <AiOutlineInstagram />
                 Instagram
-              </a>
+              </Link>
             </li>
           </div>
         </ul>
