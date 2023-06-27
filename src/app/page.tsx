d4f1a435f86e201ccde84cdf653ca66719a0414e
Mateus Capitanio <mateusjmj92@gmail.com>
2023-06-26 23:01:43 -0300

    feat: Resolvendo erros de importação da lib REACT

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Header from "@/components/Header";
 import Image from "next/image";
 
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { BsWhatsapp } from 'react-icons/bs';
 import { AiFillLinkedin, AiOutlineInstagram } from 'react-icons/ai';
 
